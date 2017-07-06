678c70a50a00e650e85baa226e0d229a6ae600d1
Stephanie Yacek <stephyacek@gmail.com>
2017-07-06 10:37:03 -0700

    Added array of comments to view.

---
+++ index.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const myNotes = [
+  {
+    id: 1,
+    comments: 'Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery.'
+  },
+  {
+    id: 2,
+    comments: 'Celery quandong swiss chard chicory earthnut pea potato.'
+  },
+  {
+    id: 3,
+    comments: 'Pea horseradish azuki bean lettuce avocado asparagus okra.'
+  }
+]
+
+app.use(bodyParser.json())
+
+app.get('/my-notes', (req, res) => {
+  res.json(myNotes)
+})
+
+app.post('/my-notes', (req, res) => {
+  myNotes.push(req.body)
+  res.sendStatus(201)
+})
+
+app.listen(3000, () => {
+  console.log('Listening on 3000')
+})
