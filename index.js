d19d8a7c870b3bbb4732e5c84e249e586822d09d
Stephanie Yacek <stephyacek@gmail.com>
2017-07-06 10:14:19 -0700

    Updated with status code response. #1

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.get('/my-notes', (req, res) => {
 
 app.post('/my-notes', (req, res) => {
   myNotes.push(req.body)
-  res.send('Successfully added note.')
+  res.sendStatus(201)
+  res.send('Successfully added note!')
 })
 
 app.listen(3000, () => {
